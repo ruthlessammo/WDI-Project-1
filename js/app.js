48679143e2a8dee61fcc3d1b378278d16454fd96
William Hilton <williamjchilton@gmail.com>
2016-10-07 13:57:16 +0100

    Both sprites moving acrosscontainer div

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 var lastKey1 = 'x';
+var lastKey2 = '.';
 var $sprite1 = $('.sprite1');
+var $sprite2 = $('.sprite2');
 
 $(document).on('keyup', function(e) {
 
@@ -12,5 +14,13 @@ $(document).on('keyup', function(e) {
     var left = parseFloat($sprite1.css("left"));
     $sprite1.css("left", (left + 10) + "px");
     lastKey1 = 'x'
+  } else if(e.key === ',' && lastKey2 === '.') {
+    var left = parseFloat($sprite2.css("left"));
+    $sprite2.css("left", (left + 10) + "px");
+    lastKey2 = ',';
+  } else if(e.key === '.' && lastKey2 === ',') {
+    var left = parseFloat($sprite2.css("left"));
+    $sprite2.css("left", (left + 10) + "px");
+    lastKey2 = '.';
   }
 });
