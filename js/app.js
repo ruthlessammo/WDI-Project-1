aa7c26aa6931a6c5e18ba09f6938cebda29699b2
William Hilton <williamjchilton@gmail.com>
2016-10-10 12:21:41 +0100

    sprite sheets now animated

--- js/app.js
+++ js/app.js
@@ -19,14 +19,14 @@ $(document).on('keyup', function(e) {
       var left = parseFloat($sprite1.css("left"));
       $sprite1.css("left", (left + 10) + "px");
       lastKey1 = 'z';
-      $sprite1.toggleClass('.left1 .right1');
+      $sprite1.toggleClass('left1').toggleClass('right1');
       counter1 = parseFloat(counter1+10);
     } else if(e.key === 'x' && lastKey1 === 'z') {
       var left = parseFloat($sprite1.css("left"));
       $sprite1.css("left", (left + 10) + "px");
       lastKey1 = 'x';
       counter1 = parseFloat(counter1+10);
-      $sprite1.toggleClass('.right1 .left1')
+      $sprite1.toggleClass('left1').toggleClass('right1');
       console.log(counter1);
     }
   }
@@ -38,11 +38,13 @@ $(document).on('keyup', function(e) {
       var left = parseFloat($sprite2.css("left"));
       $sprite2.css("left", (left + 10) + "px");
       lastKey2 = ',';
+      $sprite2.toggleClass('left2').toggleClass('right2');
       counter2 = parseFloat(counter2+10);
     } else if(e.key === '.' && lastKey2 === ',') {
       var left = parseFloat($sprite2.css("left"));
       $sprite2.css("left", (left + 10) + "px");
       lastKey2 = '.';
+      $sprite2.toggleClass('left2').toggleClass('right2');
       counter2 = parseFloat(counter2+10);
       console.log(counter2);
     }
@@ -52,15 +54,15 @@ $(document).on('keyup', function(e) {
 
 
   var getWinner = function() {
-    if(counter1 === 100 || counter2 === 100) {
+    if(counter1 === 1000 || counter2 === 1000) {
       $sprite1.removeClass('active') && $sprite2.removeClass('active');
       console.log('Winner!');
-      if(counter1 === 100) {
+      if(counter1 === 1000) {
         trumpWins++;
-      } else if(counter2 === 100) {
+      } else if(counter2 === 1000) {
         clintonWins++;
       }
-      document.getElementsByClassName('.trumpResults').innerHTML = trumpWins; document.getElementsByClassName('.hillaryResults').innerHTML = clintonWins;
+      document.getElementsByClassName('.trumpResults').textContent = trumpWins; document.getElementsByClassName('.hillaryResults').textContent = clintonWins;
     }
   }
   getWinner();
