9f3e33555860ee430c32ba70fd6b3434bb767d13
William Hilton <williamjchilton@gmail.com>
2016-10-10 15:27:04 +0100

    Scoreboard now functioning

--- js/app.js
+++ js/app.js
@@ -5,16 +5,38 @@ var lastKey2 = '.';
 var $sprite1 = $('.sprite1');
 var $sprite2 = $('.sprite2');
 
-var counter1 = "0";
-var counter2 = "0";
+var counter1 = 0;
+var counter2 = 0;
 
-var trumpWins = '0';
-var clintonWins = '0';
+var trumpWins = 0;
+var clintonWins = 0;
 
+var raceInProgress = true;
+
+var getWinner = function() {
+  var winningScore = 60;
+  var trumpResultsElements;
+  var hillaryResultsElements;
+
+  if(counter1 === winningScore || counter2 === winningScore) {
+    raceInProgress = false;
+    console.log('Winner!');
+    if(counter1 === winningScore) {
+      trumpWins++;
+    } else if(counter2 === winningScore) {
+      clintonWins++;
+    }
+    
+    trumpResultsElements = document.getElementById('trumpResults');
+    trumpResultsElements.textContent = trumpWins;
+    hillaryResultsElements = document.getElementById('hillaryResults');
+    hillaryResultsElements.textContent = clintonWins;
+  }
+};
 
 $(document).on('keyup', function(e) {
   //------------player 1's movements-----------
-  if ($sprite1.hasClass('active')) {
+  if (raceInProgress) {
     if(e.key === 'z' && lastKey1 === 'x') {
       var left = parseFloat($sprite1.css("left"));
       $sprite1.css("left", (left + 10) + "px");
@@ -29,11 +51,6 @@ $(document).on('keyup', function(e) {
       $sprite1.toggleClass('left1').toggleClass('right1');
       console.log(counter1);
     }
-  }
-
-
-  //------------player 2's movements-----------
-  if($sprite2.hasClass('active')) {
     if(e.key === ',' && lastKey2 === '.') {
       var left = parseFloat($sprite2.css("left"));
       $sprite2.css("left", (left + 10) + "px");
@@ -48,23 +65,17 @@ $(document).on('keyup', function(e) {
       counter2 = parseFloat(counter2+10);
       console.log(counter2);
     }
+    getWinner();
   }
 
 
 
 
-  var getWinner = function() {
-    if(counter1 === 1000 || counter2 === 1000) {
-      $sprite1.removeClass('active') && $sprite2.removeClass('active');
-      console.log('Winner!');
-      if(counter1 === 1000) {
-        return trumpWins++;
-      } else if(counter2 === 1000) {
-        return clintonWins++;
-      }
-      document.getElementsByClassName('.trumpResults').textContent = trumpWins; document.getElementsByClassName('.hillaryResults').textContent = clintonWins;
-    }
-  }
-  getWinner();
 
+  // var cheering = document.getElementById('cheering');
+  // cheering.src= "../audio/cheering.mp3";
+  // cheering.addEventListener('keydown', function() {
+  //   cheering.play();
+  // });
+  // cheering();
 });
